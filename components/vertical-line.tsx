06abd4acebdc314af8cd50695b5a3221944f7b18
Alef Gaigher <alef@adaline.com.br>
2024-06-19 03:22:57 -0300

    conserto

--- components/vertical-line.tsx
+++ components/vertical-line.tsx
@@ -1,5 +1,5 @@
-// components/VerticalLine.tsx
-import React from 'react';
+'use client'
+import React, { useEffect, useState } from 'react';
 
 interface Experience {
   year: string;
@@ -12,13 +12,43 @@ interface VerticalLineProps {
 }
 
 export const VerticalLine: React.FC<VerticalLineProps> = ({ experiences }) => {
+  const [visibleIndexes, setVisibleIndexes] = useState<number[]>([]);
+  
+  useEffect(() => {
+    const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const index = parseInt(entry.target.getAttribute('data-index') || '0', 10);
+          if (!visibleIndexes.includes(index)) {
+            setVisibleIndexes(prev => [...prev, index]);
+          }
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.1, // Dispara quando 10% do elemento estiver visível
+    });
+
+   
+    document.querySelectorAll('.vertical-item').forEach(element => {
+      observer.observe(element);
+    });
+
+    return () => observer.disconnect();
+  }, [visibleIndexes]);
+
   return (
     <div className="relative flex flex-col items-start">
       {/* Linha vertical */}
       <div className="absolute ml-2 w-px bg-white h-full"></div>
       {/* Pontos na linha */}
       {experiences.map((experience, index) => (
-        <div key={index} className="flex items-center my-4">
+        <div
+          key={index}
+          className={`flex items-center my-4 vertical-item ${visibleIndexes.includes(index) ? 'animate-scale-in' : ''}`}
+          data-index={index}
+        >
           <div className="w-4 h-4 bg-white rounded-full"></div>
           <div className="ml-4 text-start">
             <div className="text-white font-bold">{experience.year}</div>
--- config/experience.ts
+++ config/experience.ts
@@ -1,44 +1,44 @@
 export type Experience = typeof experience;
 
 export const experience = [
-    {
-      year: '2018',
-      title: 'Iniciei bacharel Ciência da Computação na Universidade Vila Velha',
-      description: 'Vila Velha/ES',
-    },
-    {
-      year: '2018',
-      title: 'Meu primeiro estágio como suporte de infra na Empresa Óticas Paris',
-      description: 'Vitória/ES',
-    },
-    {
-      year: '2019',
-      title: 'Estagiei como suporte N1 e Dev Front-End na empresa TechClass',
-      description: 'Vila Velha/ES',
-    },
-    {
-      year: '2020',
-      title: 'Estagiei como suporte N1 na empresa CONSYS',
-      description: 'Vila Velha/ES',
-    },
-    {
-      year: '2021',
-      title: 'Estagiei como Dev Full-Stack na empresa SOX',
-      description: 'Vila Velha/ES',
-    },
-    {
-        year: '2022',
-        title: 'Estagiei como Dev Full-Stack na empresa ADALINE',
-        description: 'Vila Velha/ES',
-    },
-    {
-        year: '2023',
-        title: 'Concluí meu bacharel em Ciência da Computação na Universidade Vila Velha',
-        description: 'Vila Velha/ES',
-    },
-    {
-        year: '2023',
-        title: 'Fui efetivado na empresa ADALINE como Dev Full-Stack',
-        description: 'Vila Velha/ES',
-    },
-  ];
+  {
+    year: '2018',
+    title: 'Início do Bacharelado em Ciência da Computação na Universidade Vila Velha',
+    description: 'Vila Velha/ES',
+  },
+  {
+    year: '2018',
+    title: 'Estagiário de Suporte de Infraestrutura na Empresa Óticas Paris',
+    description: 'Vitória/ES',
+  },
+  {
+    year: '2019',
+    title: 'Estagiário de Suporte N1 e Desenvolvedor Front-End na empresa TechClass',
+    description: 'Vila Velha/ES',
+  },
+  {
+    year: '2020',
+    title: 'Estagiário de Suporte N1 na empresa CONSYS',
+    description: 'Vila Velha/ES',
+  },
+  {
+    year: '2021',
+    title: 'Estagiário de Desenvolvimento Full-Stack na empresa SOX',
+    description: 'Vila Velha/ES',
+  },
+  {
+    year: '2022',
+    title: 'Estagiário de Desenvolvimento Full-Stack na empresa ADALINE',
+    description: 'Vila Velha/ES',
+  },
+  {
+    year: '2023',
+    title: 'Conclusão do Bacharelado em Ciência da Computação na Universidade Vila Velha',
+    description: 'Vila Velha/ES',
+  },
+  {
+    year: '2023',
+    title: 'Efetivação como Desenvolvedor Full-Stack na empresa ADALINE',
+    description: 'Vila Velha/ES',
+  },
+];
